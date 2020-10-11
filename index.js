5c76901b3babe5668bd63d1c33094dd1e48678ea
Caijie <caijie96@gmail.com>
2020-10-12 06:05:19 +0800

    Remove server side html and code

--- index.js
+++ index.js
@@ -23,37 +23,7 @@ if (!db_connection_string) {
     process.exit(1);
 }
 app.get("/", (req, res) => res.send("Hello World!"));
-app.get("/login", (req, res) => res.sendFile(__dirname + "/login.html"));
 app.use("/pets", pet_routes);
 app.use("/users", user_routes);
-
-app.get("/register", (req, res) => res.sendFile(__dirname + "/register.html"));
-app.post("/register", function (req, res) {
-    const roletitle = req.body.roletitle;
-    const username = req.body.username;
-    const password = req.body.password;
-    res.write("Your role is a: " + roletitle);
-    res.write("\n");
-    res.write("Your username is: " + username);
-    res.write("\n");
-    res.write("Your password is: " + password);
-    res.write("\n");
-    res.send();
-});
-
-app.get("/login", (req, res) => res.redirect("/"));
-app.post("/login", function (req, res) {
-    console.log(req.body);
-    const roletitle = req.body.roletitle;
-    const username = req.body.username;
-    const password = req.body.password;
-    res.write("Your role is a: " + roletitle);
-    res.write("\n");
-    res.write("Your username is: " + username);
-    res.write("\n");
-    res.write("Your password is: " + password);
-    res.write("\n");
-    res.send();
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
