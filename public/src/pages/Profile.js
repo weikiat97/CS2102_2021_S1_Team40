73fd422377b293a368afe7f37f1cb771803375ce
Caijie <caijie96@gmail.com>
2020-10-21 02:14:24 +0800

    Use container for leave and profile

--- public/src/pages/Profile.js
+++ public/src/pages/Profile.js
@@ -2,23 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectUser } from "../redux/slices/userSlice";
+import Container from "@material-ui/core/Container";
 
 export default function Profile() {
   const user = useSelector(selectUser);
   if (user) {
     return (
-      <div>
+      <Container>
         <h1>YOU ARE AT THE PROFILE PAGE</h1>
         <Link to="/profile/leaves">
           <button>Go to Leaves</button>
         </Link>
-      </div>
+      </Container>
     );
   } else {
     return (
-      <div>
+      <Container>
         <h1>Please Login Create an account with us if you haven't!</h1>
-      </div>
+      </Container>
     );
   }
 }
