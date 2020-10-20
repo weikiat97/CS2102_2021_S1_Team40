23a61e0abbec27461a0f4214f4add219224e31ca
weikiat97 <jerryloh97@hotmail.com>
2020-10-20 21:50:41 +0800

    did some stuff

--- public/src/pages/Profile.js
+++ public/src/pages/Profile.js
@@ -5,15 +5,22 @@ import { selectUser } from "../redux/slices/userSlice";
 
 export default function Profile() {
   const user = useSelector(selectUser);
+  console.log(user);
   if (user) {
     return (
       <div>
         <h1>YOU ARE AT THE PROFILE PAGE</h1>
-        <Link to="/profile/leaves">
-          <button>Go to Leaves</button>
-        </Link>
+          <Link to="/profile/leaves">
+            <button>Go to Leaves</button>
+          </Link>
       </div>
     );
+  // } else if (user) {
+  //   return (
+  //     <div>
+  //       <h1>YOU ARE AT THE PROFILE PAGE</h1>
+  //     </div>
+  //   );
   } else {
     return (
       <div>
