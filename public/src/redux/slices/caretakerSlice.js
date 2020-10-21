f0331687762f16003338414f9884f9be786402c6
Sheryl Tay <sheryltayyyy@gmail.com>
2020-10-22 00:07:13 +0800

    still not working

--- public/src/redux/slices/caretakerSlice.js
+++ public/src/redux/slices/caretakerSlice.js
@@ -14,6 +14,31 @@ export const careTakerSlice = createSlice({
   },
 });
 
+export const getCaretakers = (maximum_price, pet_type, start_date, end_date) => (dispatch) => {
+  fetch(`${API_HOST}/users/find-caretakers`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "GET",
+    body: JSON.stringify({
+      advertised_price: maximum_price,
+      pet_type: pet_type,
+      start_date: start_date,
+      end_date: end_date,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.status === "success") {
+        dispatch(setCareTaker(result.data));
+      } else {
+        throw new Error(result.message);
+      }
+    })
+    .catch((err) => alert(err));
+};
+
+
 export const { setCareTaker, setBasicInfo } = careTakerSlice.actions;
 
 export const getCareTakerFromDb = (username, password) => (dispatch) => {
