c8520fe5dcf82e18cea83f54fa57d398d42642e1
Sheryl Tay <sheryltayyyy@gmail.com>
2020-10-21 11:33:43 +0800

    updated caretakerslice

--- public/src/redux/slices/caretakerSlice.js
+++ public/src/redux/slices/caretakerSlice.js
@@ -10,36 +10,37 @@ export const careTakerSlice = createSlice({
   initialState: persistedCareTaker,
   reducers: {
     setCareTaker: (state, action) => action.payload,
+    setBasicInfo: (state, action) => {return {...state, ...action.payload}}
   },
 });
 
-export const { setCareTaker } = careTakerSlice.actions;
-
 export const getCaretakers = (maximum_price, pet_type, start_date, end_date) => (dispatch) => {
-    fetch(`${API_HOST}/users/find-caretakers`, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "GET",
-      body: JSON.stringify({
-        advertised_price: maximum_price,
-        pet_type: pet_type,
-        start_date: start_date,
-        end_date: end_date,
-      }),
+  fetch(`${API_HOST}/users/find-caretakers`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "GET",
+    body: JSON.stringify({
+      advertised_price: maximum_price,
+      pet_type: pet_type,
+      start_date: start_date,
+      end_date: end_date,
+    }),
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.status === "success") {
+        saveState(CARETAKER_STATE_KEY, result.data);
+        dispatch(setCareTaker(result.data));
+      } else {
+        throw new Error(result.message);
+      }
     })
-      .then((response) => response.json())
-      .then((result) => {
-        if (result.status === "success") {
-          saveState(CARETAKER_STATE_KEY, result.data);
-          dispatch(setCareTaker(result.data));
-        } else {
-          throw new Error(result.message);
-        }
-      })
-      .catch((err) => alert(err));
-  };
+    .catch((err) => alert(err));
+};
 
+
+export const { setCareTaker, setBasicInfo } = careTakerSlice.actions;
 
 export const getCareTakerFromDb = (username, password) => (dispatch) => {
   fetch(`${API_HOST}/caretakers/${username}`, {
@@ -85,6 +86,24 @@ export const signupCareTaker = (username, password) => (dispatch) => {
     })
     .catch((err) => alert(err));
 };
+
+export const getCareTakerBasicInfo = (username) => (dispatch) => {
+  fetch(`${API_HOST}/caretakers/${username}`, {
+    headers: {
+      "Content-Type": "application/json",
+      method: "GET",
+    }
+  })
+    .then(response => response.json())
+    .then(result => {
+      if (result.status === "success") {
+        dispatch(setBasicInfo(result.data));
+      } else {
+        throw new Error(result.message);
+      }
+    })
+    .catch(err => alert(err));
+}
 
 export const selectCareTaker = (state) => state.caretaker;
 
