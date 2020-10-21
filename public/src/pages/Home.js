f940afec5707568e70e8279a5db011f926f62d05
Caijie <caijie96@gmail.com>
2020-10-21 22:43:18 +0800

    Wrap home page in Container

--- public/src/pages/Home.js
+++ public/src/pages/Home.js
@@ -2,41 +2,43 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "../redux/slices/userSlice";
 import { selectCareTaker } from "../redux/slices/careTakerSlice";
+import Container from "@material-ui/core/Container";
 
 export default function Home() {
   const user = useSelector(selectUser);
   const caretaker = useSelector(selectCareTaker);
   if (user && caretaker) {
     return (
-      <div>
+      <Container>
         <h1>
           Welcome {user.username}. You are registered as both a petowner and a
           caretaker. Would you like to be a parttime caretaker or fulltime
           caretaker?
         </h1>
-      </div>
+      </Container>
     );
   } else if (user) {
     return (
-      <div>
+      <Container>
         <h1>Welcome {user.username}. You are registered as a petowner.</h1>
-      </div>
+      </Container>
     );
   } else if (caretaker) {
     return (
-      <div>
+      <Container>
         <h1>
           Welcome {caretaker.username}. You are registered as a caretaker. Would
           you like to be a parttime caretaker or fulltime caretaker?
         </h1>
-      </div>
+      </Container>
     );
   } else {
     return (
-      <div>
+      <Container>
         <br></br>
-        <h1>Please Login. Create an account with us if you haven't!</h1>
-      </div>
+        <h1>Please Login.</h1>
+        <h2>Create an account with us if you haven't!</h2>
+      </Container>
     );
   }
 }
