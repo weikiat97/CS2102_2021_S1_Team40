997f8b6d7db0a1ada7b4ec90a88a5e2aa9085e0f
Sheryl Tay <sheryltayyyy@gmail.com>
2020-10-23 02:09:03 +0800

    caretakers show up now

--- public/src/pages/Caretakers.js
+++ public/src/pages/Caretakers.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import { API_HOST } from "../consts";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -11,34 +12,35 @@ import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import { Link } from "react-router-dom";
 import Bid from "../components/Bid";
+import { selectCareTaker } from "../redux/slices/careTakerSlice";
 
 export default function Caretakers() {
     const [bid_page_open, setBidPageOpen] = useState(false);
-    const [caretakers, setCaretakers] = useState([]);
+    //const [caretakers, setCaretakers] = useState([]);
+    const caretakers = useSelector(selectCareTaker);
 
-    useEffect(() => {
-        async function fetchData() {
-            await fetch(`${API_HOST}/users/find-caretakers`, {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                method: "POST",
-            })
-                .then((response) => response.json())
-                .then((response) => console.log(response))
-                .then(async (result) => {
-                    if (result.status === "success") {
-                        console.log("yay");
-                        await setCaretakers(result.data);
-                    } else {
-                        console.log("No Caretakers found");
-                    }
-                })
-                .catch((err) => alert(err));
-        }
-        fetchData();
-    }, []);
-    console.log("why not showing" + caretakers.toString());
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         await fetch(`${API_HOST}/users/find-caretakers`, {
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //             method: "POST",
+    //         })
+    //             .then((response) => response.json())
+    //             .then((response) => console.log(response))
+    //             .then(async (result) => {
+    //                 if (result.status === "success") {
+    //                     console.log("yay");
+    //                     await setCaretakers(result.data);
+    //                 } else {
+    //                     console.log("No Caretakers found");
+    //                 }
+    //             })
+    //             .catch((err) => alert(err));
+    //     }
+    //     fetchData();
+    // }, []);
     return (
         <div>
             <h1>List of Caretakers that match your criteria</h1>
