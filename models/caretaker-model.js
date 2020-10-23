c0a9bb2227f12906836c6b52ced142355e570da9
Tay Sheryl <50551102+sheryltay28@users.noreply.github.com>
2020-10-24 01:14:46 +0800

    removed commit logs

--- models/caretaker-model.js
+++ models/caretaker-model.js
@@ -11,11 +11,6 @@ class Caretaker {
     }
     //also need to consider only those bids that are during the same time period
     async getRequiredCaretakers(maximum_price, pet_type, start_date, end_date) {
-        console.log("hihi im here");
-        console.log(start_date);
-        console.log(maximum_price);
-        console.log(pet_type);
-        console.log(end_date);
         let query = `SELECT username, advertised_price, start_date, end_date
                     FROM availabilities
                     WHERE start_date <= '${start_date}' AND end_date >= '${end_date}'
@@ -36,10 +31,8 @@ class Caretaker {
                                                                 END
                                                             END) B
                     WHERE   A.username = B.caretaker_username AND A.start_date <= '${end_date}' AND A.end_date >= '${start_date}'`;
-        console.log("byebye")
         const results = await this.pool.query(query);
         if (results.rows.length === 0) {
-            console.log("something went wrong with query");
             return null;
         } else {
             console.log("query went right: " + JSON.stringify(results));
